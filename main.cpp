250d5650f1d5978c6351068942d2d766cdd57134
karrivinay54 <vinaykarri54@gmail.com>
2026-07-07 16:06:52 +0530

    Initial project setup

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "=================================\n";
+    cout << "        TIC TAC TOE GAME\n";
+    cout << "=================================\n";
+
+    return 0;
+}
